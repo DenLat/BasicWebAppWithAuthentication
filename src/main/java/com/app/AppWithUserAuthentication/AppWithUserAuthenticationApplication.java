ef616f059fd82fe7e5f45c26af10f4c278981028
Denys Kostiuk <65499981+DenLat@users.noreply.github.com>
2024-09-25 17:46:21 +0200

    Initial commit

---
+++ src/main/java/com/app/AppWithUserAuthentication/AppWithUserAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.app.AppWithUserAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithUserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithUserAuthenticationApplication.class, args);
+	}
+
+}
